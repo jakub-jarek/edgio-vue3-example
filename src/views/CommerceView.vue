3f759e567ebf0a5ecc06d614368ab0f8c009c60b
Jakub Jarek <j.jarek@outlook.com>
2023-01-27 11:10:38 +0100

    Prettified

--- src/views/CommerceView.vue
+++ src/views/CommerceView.vue
@@ -5,21 +5,30 @@
         <Sidebar />
       </div>
       <div class="flex flex-col items-start pt-5">
-        <h2 class="text-[#FFFFFF75]">Showing {{ items? items.length : '...' }} Results</h2>
+        <h2 class="text-[#FFFFFF75]">Showing {{ items ? items.length : '...' }} Results</h2>
         <div class="mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-          <router-link :key="item.path" v-for="item in items || fallbackData" :to="`/product/${item.slug}`"
-            class="relative mt-2 border border-white p-1">
+          <router-link
+            :key="item.path"
+            v-for="item in items || fallbackData"
+            :to="`/product/${item.slug}`"
+            class="relative mt-2 border border-white p-1"
+          >
             <div class="absolute top-0 left-0 z-10 flex flex-col items-start">
-              <h3
-                class="border border-gray-200 bg-white py-1 px-2 text-xs md:py-2 md:px-4 md:text-xl font-medium text-black">
+              <h3 class="border border-gray-200 bg-white py-1 px-2 text-xs md:py-2 md:px-4 md:text-xl font-medium text-black">
                 {{ item.name }}
               </h3>
               <h4 class="border border-gray-200 bg-white py-1 px-2 text-xs md:py-2 md:px-4 md:text-lg text-black">
                 {{ item.prices.price.value }}{{ item.prices.price.currencyCode }}
               </h4>
             </div>
-            <img width="1200" height="1200" loading="lazy" v-if="item.images" :src="getImage(item.images[0].url)"
-              class="h-full object-contain bg-white" />
+            <img
+              width="1200"
+              height="1200"
+              loading="lazy"
+              v-if="item.images"
+              :src="getImage(item.images[0].url)"
+              class="h-full object-contain bg-white"
+            />
             <div v-if="!item.images" class="h-full w-full bg-white/50 animate-pulse min-h-[100px] min-w-[100px]"></div>
             <Prefetch :url="`/edgio-api/products/${item.slug}`"><span class="w-0 h-0"></span></Prefetch>
           </router-link>
