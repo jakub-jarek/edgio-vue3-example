a86d584749a37cf0797c11c17ac8c358b41850a3
Jakub Jarek <j.jarek@outlook.com>
2023-01-27 15:10:20 +0100

    cleanup

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import './assets/styles.css'
 
 import { Metrics } from '@edgio/rum'
-import { install } from '@edgio/prefetch/window'
 import installDevtools from '@edgio/devtools/install'
 import Router from '@edgio/rum/Router.js'
 
@@ -13,7 +12,6 @@ app.use(router)
 app.mount('#app')
 
 installDevtools()
-install({ includeCacheMisses: true })
 new Metrics({
   token: '789a5742-3d8d-4894-90ec-6ca44b91f914',
   router: new Router()
